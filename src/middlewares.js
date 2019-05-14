f290fde71ab6e6a174d78f235a1acf3621414cfe
Albert Rothman <albertwrothman@gmail.com>
2019-05-13 22:40:35 -0700

    basic frontend completed

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,17 +1,21 @@
 const { User } = require('./db/models');
 const uuidAPIKey = require('uuid-apikey');
 async function authorized (req, res, next) {
-  console.log(req.headers);
-  const uuid = uuidAPIKey.toUUID(req.headers.authorization.split(' ')[1]);
-  const users = await User.findAll({ where: {
-    apikey: uuid
-  } });
-  if (!users.length) {
-    res.status(401);
-    res.send('fail');
-  } else {
-    req.userId = users[0].id;
-    next();
+  try {
+    const uuid = uuidAPIKey.toUUID(req.headers.authorization.split(' ')[1]);
+    const users = await User.findAll({ where: {
+      apikey: uuid
+    } });
+    if (!users.length) {
+      res.status(401);
+      res.send('fail');
+    } else {
+      req.userId = users[0].id;
+      next();
+    }
+  } catch (e) {
+    res.status(500);
+    res.send('Malformed request');
   }
 }
 
